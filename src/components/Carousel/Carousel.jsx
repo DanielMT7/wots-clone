44983dde61856735bc0b5168ac9696bad2f607ac
Daniel Motta <danielmotta04@hotmail.com>
2024-01-25 22:14:34 -0300

    feature: adiciona novo componente de carrosel

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,48 @@
+import React, { useRef } from 'react'
+
+import styles from './Carousel.module.css'
+
+import arrow from '../../assets/images/arrow.png'
+
+function Carousel({ data }) {
+  const { clothes } = data
+  const carousel = useRef(null)
+
+  const handleLeftClick = e => {
+    e.preventDefault()
+    carousel.current.scrollLeft -= carousel.current.offsetWidth
+  }
+
+  const handleRightClick = e => {
+    e.preventDefault()
+
+    carousel.current.scrollLeft += carousel.current.offsetWidth
+  }
+
+  console.log(clothes)
+
+  return (
+    <div className={styles.container_carousel}>
+      <button className={styles.leftArrow} onClick={handleLeftClick}>
+        <img src={arrow} alt="" />
+      </button>
+      <div className={styles.carousel} ref={carousel}>
+        {clothes.map(clothe => {
+          const { image, price, brand } = clothe
+          return (
+            <div className={styles.info}>
+              <img className={styles.item} src={image} />
+              <h1>{brand}</h1>
+              <span>R${price}</span>
+            </div>
+          )
+        })}
+      </div>
+      <button className={styles.rightArrow} onClick={handleRightClick}>
+        <img src={arrow} alt="" />
+      </button>
+    </div>
+  )
+}
+
+export default Carousel
--- src/routes/Home/components/Card/Card.jsx
+++ src/routes/Home/components/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import ClotheCardList from '../ClotheCardList/ClotheCardList'
 import Slider from '../../../../components/Slider/Slider'
@@ -6,13 +6,27 @@ import Slider from '../../../../components/Slider/Slider'
 import styles from './Card.module.css'
 
 import heart from '../../../../assets/images/coracao.png'
+import Carousel from '../../../../components/Carousel/Carousel'
 
 function Card({ data }) {
   const { outfitImage, profileImage, nickName, date, clothes } = data
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 600)
 
   let sum = clothes.reduce((accumulator, object) => {
     return accumulator + object.price
   }, 0)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 600)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   return (
     <div className={styles.card_container}>
@@ -20,43 +34,67 @@ function Card({ data }) {
         <Slider images={outfitImage} />
       </div>
 
-      <div className={styles.profile_container}>
-        <div className={styles.info_profile_container}>
-          <div className={styles.image_profile_container}>
-            <img src={profileImage} alt="" />
-          </div>
-
-          <div className={styles.more_info_profile_container}>
-            <div className={styles.name_date_profile_container}>
-              <h1>{nickName}</h1>
-              <h2>{date}</h2>
+      {isSmallScreen ? (
+        <>
+          <div className={styles.info_profile_container}>
+            <div className={styles.image_profile_container}>
+              <img src={profileImage} alt="" />
             </div>
 
-            <div className={styles.heartIcon}>
-              <img src={heart} alt="" />
+            <div className={styles.more_info_profile_container}>
+              <div className={styles.name_date_profile_container}>
+                <h1>{nickName}</h1>
+                <h2>{date}</h2>
+              </div>
+
+              <div className={styles.heartIcon}>
+                <img src={heart} alt="" />
+              </div>
+            </div>
+          </div>
+          <div className={styles.container_carousel}>
+            <Carousel data={data} />
+          </div>
+        </>
+      ) : (
+        <div className={styles.profile_container}>
+          <div className={styles.info_profile_container}>
+            <div className={styles.image_profile_container}>
+              <img src={profileImage} alt="" />
+            </div>
+
+            <div className={styles.more_info_profile_container}>
+              <div className={styles.name_date_profile_container}>
+                <h1>{nickName}</h1>
+                <h2>{date}</h2>
+              </div>
+
+              <div className={styles.heartIcon}>
+                <img src={heart} alt="" />
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.all_clothes_container}>
+            {clothes.map((clothe, index) => {
+              return (
+                <ClotheCardList
+                  key={index}
+                  id={clothe.id}
+                  image={clothe.image}
+                  brand={clothe.brand}
+                  clotheName={clothe.clotheName}
+                  price={clothe.price}
+                />
+              )
+            })}
+            <div className={styles.full_clothes_price}>
+              <h1>Total:</h1>
+              <span>R${sum}</span>
             </div>
           </div>
         </div>
-
-        <div className={styles.all_clothes_container}>
-          {clothes.map((clothe, index) => {
-            return (
-              <ClotheCardList
-                key={index}
-                id={clothe.id}
-                image={clothe.image}
-                brand={clothe.brand}
-                clotheName={clothe.clotheName}
-                price={clothe.price}
-              />
-            )
-          })}
-          <div className={styles.full_clothes_price}>
-            <h1>Total:</h1>
-            <span>R${sum}</span>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   )
 }
